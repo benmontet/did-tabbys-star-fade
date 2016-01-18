2c3ed9d7285947ec7bd24ddffc990db40e3c4424
Geert Barentsen <geert@barentsen.be>
2016-01-17 17:08:02 -0800

    Improved summary table

--- data/README.md
+++ data/README.md
@@ -5,3 +5,5 @@ DASCH data for Tabby's star was obtained from:
 http://dasch.rc.fas.harvard.edu/tmp/UqlaOr/20:06:15.457_+44:27:24.61_APASS_J200615.5+442725_0001.xml.gz
 
 The DASCH data is composed of 19 different "plate series".
+
+Pictures of the telescopes are [on the DASCH website](http://dasch.rc.fas.harvard.edu/telescopes.php), they may be fun to include in the paper.
--- data/split-data-by-instrument.py
+++ data/split-data-by-instrument.py
@@ -15,18 +15,21 @@ if __name__ == "__main__":
 
     summary = []
     for seriesid in series_ids:
-        mask = tbl["seriesId"] == seriesid
-        tbl[mask].write("series/series-{}.csv".format(seriesid), format="ascii.csv")
-        summary.append({
-                                'seriesId': seriesid,
-                                'year_begin': jd2year(tbl[mask]["ExposureDate"].min()),
-                                'year_end': jd2year(tbl[mask]["ExposureDate"].max()),
-                                'datapoints' : mask.sum()
-                        })
+        # AFLAGS are described here: http://dasch.rc.fas.harvard.edu/database.php#AFLAGS_ext
+        # Schaefer rejects data points with AFLAGS > 9000 
+        mask = (tbl["seriesId"] == seriesid) & (tbl["AFLAGS"] <= 9000)
+        if mask.sum() > 0:
+            tbl[mask].write("series/series-{}.csv".format(seriesid), format="ascii.csv")
+            summary.append({
+                                    'seriesId': seriesid,
+                                    'year_begin': jd2year(tbl[mask]["ExposureDate"].min()),
+                                    'year_end': jd2year(tbl[mask]["ExposureDate"].max()),
+                                    'good_datapoints' : mask.sum()
+                            })
 
     summary_tbl = Table(summary)
     summary_tbl = join(summary_tbl, series_tbl,
                        keys="seriesId", join_type="left")
-    summary_tbl.sort("datapoints")
+    summary_tbl.sort("good_datapoints")
     summary_tbl.reverse()
-    summary_tbl["series", "seriesId", "datapoints", "year_begin", "year_end", "aperture", "telescope"].write("dasch-data-summary.csv", format="ascii.csv")
+    summary_tbl["series", "seriesId", "good_datapoints", "year_begin", "year_end", "aperture", "telescope"].write("dasch-data-summary.csv", format="ascii.csv")
